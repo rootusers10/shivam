aceb31b05984833d9dc8d0e314ad56d35291d565
hp <hp@DESKTOP-DOQTNLA>
2024-08-27 03:37:05 -0700

    jsj

---
+++ src/shivam1/Bike.java
@@ -0,0 +1,13 @@
+package shivam1;
+
+public class Bike {
+	abstract void run();
+		}
+		class Honda4 extends Bike{
+		void run(){System.out.println("running safely");}
+		public static void main(String args[]){
+		Bike obj=new Honda4();
+		obj.run();
+		}
+		
+}
